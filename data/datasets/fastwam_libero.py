e1395d61725618ad79439b0c4092f3cedd706a79
ReyChiaro <1402850762@qq.com>
2026-08-31 03:08:14 +0800

    update fastwam_libero dataset

--- data/datasets/fastwam_libero.py
+++ data/datasets/fastwam_libero.py
@@ -1,16 +1,43 @@
 import json
+import torch
 import numpy as np
+import torchvision.transforms.functional as T
 
-from collections import defaultdict
-from pathlib import Path
-from typing import Any
-from torch.utils.data import default_collate
-from typing import Literal
+from bisect import bisect_right
 from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Any, Literal
+
+from torch.utils.data import default_collate
 
 from data.datasets.base import BaseDataset
+from data.utils.preprocess import decode_video, load_parquet
 
 PROMPT_TEMPLATE = "A video recorded from a robot's point of view executing the following instruction: {task}"
+
+
+@dataclass
+class DataStats:
+
+    minimum: np.ndarray
+    maximum: np.ndarray
+    mean: np.ndarray
+    std: np.ndarray
+    q01: np.ndarray
+    q99: np.ndarray
+
+    @classmethod
+    def from_path(cls, path: str, key: str) -> "DataStats":
+        with open(path) as f:
+            stats = json.load(f).get(key)["default"]
+        return cls(
+            minimum=np.array(stats["global_min"], dtype=np.float32),
+            maximum=np.array(stats["global_max"], dtype=np.float32),
+            mean=np.array(stats["global_mean"], dtype=np.float32),
+            std=np.array(stats["global_std"], dtype=np.float32),
+            q01=np.array(stats["global_q01"], dtype=np.float32),
+            q99=np.array(stats["global_q99"], dtype=np.float32),
+        )
 
 
 @dataclass
@@ -21,6 +48,17 @@ class FastWAMLiberoSplitMeta:
     num_chunks: int
     num_episodes: int
     fps: int
+
+    @classmethod
+    def from_path(cls, path: str) -> "FastWAMLiberoSplitMeta":
+        with open(path / "meta" / "info.json") as f:
+            info = json.load(f)
+        return FastWAMLiberoSplitMeta(
+            path=path,
+            num_chunks=info.get("total_chunks"),
+            num_episodes=info.get("total_episodes"),
+            fps=info.get("fps"),
+        )
 
 
 @dataclass
@@ -73,42 +111,56 @@ class FastWAMLiberoDataset(BaseDataset):
         self,
         root: str,
         splits: list[Literal["libero_10", "libero_goal", "libero_object", "libero_spatial"]],
+        dataset_stats: str,
+        video_size: tuple[int] = [224, 224],
+        action_horizon: int = 32,
+        video_stride: int = 4,
         camera_keys: list[str] = ["image", "wrist_image"],
+        action_key: str = "action",
+        proprio_key: str = "observation.state",
     ):
+        r"""
+        Args:
+            action_horizon (int): Num of action frames in one window, default 32
+            video_stride (int): Video frames sampling frequence, default 4
+            dataset_stats (str): The statistics of datasets for all given splits, should be pre-computed.
+        """
         super().__init__()
 
         root = Path(root)
         splits = set(splits)
 
         self.splits = splits
+        self.video_size = video_size
+        self.action_horizon = action_horizon
+        self.video_stride = video_stride
         self.camera_keys = camera_keys
+        self.action_key = action_key
+        self.proprio_key = proprio_key
+
         self.split_meta_info: dict[str, FastWAMLiberoSplitMeta] = {}
         self.episodes: list[FastWAMLiberoEpisode] = []
 
+        self.action_stats = DataStats.from_path(dataset_stats, "action")
+        self.proprio_stats = DataStats.from_path(dataset_stats, "state")
+
         for split in self.splits:
             split_path = root / split
-            with open(split_path / "meta" / "info.json") as f:
-                info = json.load(f)
-                self.split_meta_info[split] = FastWAMLiberoSplitMeta(
-                    path=split_path,
-                    num_chunks=info.get("total_chunks"),
-                    num_episodes=info.get("total_episodes"),
-                    fps=info.get("fps"),
-                )
+            self.split_meta_info[split] = FastWAMLiberoSplitMeta.from_path(split_path)
 
             episode_map = {}
             with open(split_path / "meta" / "episodes.jsonl") as f:
                 for line in f:
-                    mapping = json.load(line)
+                    mapping = json.loads(line)
                     episode_map[mapping.get("episode_index")] = {
-                        "task": mapping.get("task")[0],
+                        "task": mapping.get("tasks")[0],
                         "length": mapping.get("length"),
                     }
 
             task_map = {}
             with open(split_path / "meta" / "tasks.jsonl") as f:
                 for line in f:
-                    mapping = json.load(line)
+                    mapping = json.loads(line)
                     task_map[mapping.get("task_index")] = mapping.get("task")
 
             meta = self.split_meta_info[split]
@@ -129,8 +181,65 @@ class FastWAMLiberoDataset(BaseDataset):
                         )
                     )
 
+        self._episode_end_indices = np.cumsum([e.num_frames for e in self.episodes]).tolist()
+
     def __len__(self) -> int:
         return len(self.episodes)
 
+    def concat_videos(self, videos: list[torch.Tensor]) -> torch.Tensor:
+        videos = [
+            T.resize(v, self.video_size, interpolation=T.InterpolationMode.BICUBIC).clamp(0.0, 1.0) for v in videos
+        ]
+        return torch.cat(videos, dim=-1)
+
+    def normalize(self, values: torch.Tensor, stats: DataStats) -> torch.Tensor:
+        minimum = torch.from_numpy(stats.minimum).to(values)
+        maximum = torch.from_numpy(stats.maximum).to(values)
+        value_range = maximum - minimum
+        scale = torch.where(value_range < 1e-4, torch.ones_like(value_range), 2 / value_range)
+        offset = torch.where(value_range < 1e-4, -minimum, -1 - scale * minimum)
+        return (values * scale + offset).clamp(-5, 5)
+
+    def denormalize(self, values: torch.Tensor, stats: DataStats) -> torch.Tensor:
+        minimum = torch.from_numpy(stats.minimum).to(values)
+        maximum = torch.from_numpy(stats.maximum).to(values)
+        value_range = maximum - minimum
+        scale = torch.where(value_range < 1e-4, torch.ones_like(value_range), 2 / value_range)
+        offset = torch.where(value_range < 1e-4, -minimum, -1 - scale * minimum)
+        return (values - offset) / scale
+
     def __getitem__(self, frame_index: int) -> dict[str, Any]:
-        pass
+        # Find the corresponding episode
+        episode_index = bisect_right(self._episode_end_indices, frame_index)
+        frame_start = 0 if episode_index == 0 else self._episode_end_indices[episode_index - 1]
+        episode = self.episodes[episode_index]
+
+        offset = frame_index - frame_start
+        num_frames = episode.num_frames
+
+        # Config frames/indices to load and which are padding
+        action_indices = offset + list(range(self.action_horizon))
+        video_indices = offset + list(range(0, self.action_horizon + 1, self.video_stride))
+        action_pad = action_indices >= num_frames
+        video_pad = video_indices >= num_frames
+
+        # Load videos, actions and states
+        # Videos from different cameras are concatenated horizontally and sequentially
+        videos = self.concat_videos([decode_video(episode.video_paths[k]) for k in self.camera_keys])
+        states = load_parquet(episode.state_path, columns=[self.action_key, self.proprio_key])
+
+        # Preprocess
+        prompt = PROMPT_TEMPLATE.format_map({"task": episode.task_instruct})
+        actions = self.normalize(states[self.action_key], self.action_stats)
+        # Only first proprio state in the horizon is used
+        proprios = self.normalize(states[self.proprio_stats][0], self.proprio_stats)
+
+        # Note that the returned dict have the same keys defined in self._keys_
+        return {
+            "prompt": prompt,
+            "video": videos,
+            "action": actions,
+            "proprio_states": proprios,
+            "video_pad": video_pad,
+            "action_pad": action_pad,
+        }
--- data/utils/preprocess.py
+++ data/utils/preprocess.py
@@ -1,2 +1,29 @@
-# Preprocessor
+import torch
+import pyarrow
+import numpy as np
 
+from typing import Optional
+from pathlib import Path
+from torchcodec.decoders import VideoDecoder
+from pyarrow import parquet
+
+
+def decode_video(mp4_path: Path, fps: float, frame_indices: Optional[list[int]] = None) -> torch.Tensor:
+    # Use ffmpeg
+    decoder = VideoDecoder(mp4_path, device="cpu", seek_mode="approximate")
+    if frame_indices is None:
+        frames = decoder.get_all_frames(fps=fps).data
+    else:
+        frames = decoder.get_frames_at(frame_indices).data
+    return frames.to(dtype=torch.float32)
+
+
+def load_parquet(parquet_path: Path, columns: Optional[list[str]]) -> dict[str, torch.Tensor]:
+    table: pyarrow.Table = parquet.read_table(parquet_path, columns=columns)
+    column_tensors = {}
+    for name in columns:
+        array = table[name].combine_chunks().to_numpy(zero_copy_only=False)
+        if array.dtype == object:
+            array = np.stack(array)
+        column_tensors[name] = torch.from_numpy(np.asarray(array).copy()).to(dtype=torch.float32)
+    return column_tensors
